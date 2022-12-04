c14e08362ba3f3ff81fc528a0cb1fefcf5e4c475
nirav2940 <mt_0154@outlook.com>
2022-12-04 18:31:17 +0530

    change data

--- config/database.js
+++ config/database.js
@@ -1,36 +1,36 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
 var isLocal = false;
 var con;
 if (isLocal) {
   con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    port: '3306',
-    password: '',
-    database: 'khethisab',
+    host: "localhost",
+    user: "root",
+    port: "3306",
+    password: "",
+    database: "khethisab",
     multipleStatements: true,
-    charset: 'utf8mb4',
+    charset: "utf8mb4",
   });
 } else {
-  // con = mysql.createConnection({
-  //     host: "50.62.209.15",
-  //     port: '3306',
-  //     user: "nirav",
-  //     password: "nirav.kumar90914",
-  //     database: "ph11691450941_",
-  //     multipleStatements: true,
-  //     charset: 'utf8mb4'
-  // });
-
   con = mysql.createConnection({
-    host: '216.10.252.35',
-    port: '3306',
-    user: 'keshauqj_keshauqj',
-    password: 'UmangC~12!',
-    database: 'keshauqj_khethisab',
+    host: "50.62.209.15",
+    port: "3306",
+    user: "nirav",
+    password: "nirav.kumar90914",
+    database: "ph11691450941_",
     multipleStatements: true,
-    charset: 'utf8mb4',
+    charset: "utf8mb4",
   });
+
+  // con = mysql.createConnection({
+  //   host: '216.10.252.35',
+  //   port: '3306',
+  //   user: 'keshauqj_keshauqj',
+  //   password: 'UmangC~12!',
+  //   database: 'keshauqj_khethisab',
+  //   multipleStatements: true,
+  //   charset: 'utf8mb4',
+  // });
 }
 
 module.exports = con;
